372f4f978f5876048fd8d9a6d747e99d0d6052e0
nate2411 <Nathandj28@gmail.com>
2021-04-15 12:42:19 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+#task 1
+#Right angle triangle
+ab = float(input("please enter the first side:"))
+bc = float(input("please enter the second side:"))
+
+ac = (ab**2 + bc**2)**(1/2)
+print("the third side of the triangle equals: " + str(ac))
+
+#task 2
+mynumber = [2,56,12,67,1000,32,89,29,44,39,200,11,21]
+print("Max value element: " + str(max(mynumber)))
+print("Min value element: " + str(min(mynumber)))
+
+
+
+
+
+
+
